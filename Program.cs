d05c914322b7f83a804d3e2cc172e1589ec1b077
stValentyn <valik7km@gmail.com>
2022-03-07 20:24:21 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace TCPlistener
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tcp = new TcpListener(IPAddress.Parse("127.0.0.1"), 9090);
+
+            tcp.Start(32);
+
+            var buf = new byte[512];
+            using var tcpClient = tcp.AcceptTcpClient();
+            tcpClient.GetStream().Read(buf, 32, 32);
+
+            var data = Encoding.ASCII.GetString(buf);
+            Console.WriteLine(data);
+
+            tcp.Stop();
+
+        }
+    }
+}
